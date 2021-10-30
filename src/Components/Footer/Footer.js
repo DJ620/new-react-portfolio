60d4eb6fe8b2e6677a5b872b3fbe310281dd44c9
DJ Hersh <DanielJHersh@gmail.com>
2021-10-30 12:46:30 -0700

    Fixed broken Github logo

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import twitter from "../../images/twitter.png";
 import logo from "../../images/logo192.png";
+import github from "../../images/Icons/githubIcon.png";
 import "./Footer.css";
 
 function Footer() {
@@ -11,7 +12,7 @@ function Footer() {
                   <img src="https://cdn2.iconfinder.com/data/icons/linkedin-ui-flat/48/LinkedIn_UI-02-512.png" alt="linkedin" className="linkedin"/>
               </a>
               <a href="https://github.com/DJ620" target="_blank">
-                  <img src="https://www.flaticon.com/svg/static/icons/svg/25/25231.svg" alt="github" className="github mt-3"/>
+                  <img src={github} alt="github" className="github mt-3"/>
               </a>
               <a href="https://twitter.com/DjhershWebDev" target="_blank">
                   <img src={twitter} alt="twitter" className="twitter mt-1 ml-1"/>
