2d7b4c59937996db086498e9053e0ee227ef2934
DJ Hersh <DanielJHersh@gmail.com>
2023-10-17 15:50:53 -0700

    updating portfolio

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -33,14 +33,17 @@ function About() {
           Before I decided to jump into the world of coding, I was living in New York City pursuing a career as an actor and working as a server (an actor working as a server? Never...) at Ellen's Stardust Diner. I wasn't just an ordinary server, I was a SINGING server. That's right, while I would go off to fetch your Coca-cola, I would come back with a microphone in my other hand belting out 50's songs and showtunes. It was a fun time! Once the world kinda fell into a dark pit of despair (you know what I'm talking about), I realized that I wanted stability and structure in my life. 
           </h5>
           <h5 className="text-white mb-5" style={styles.main}>
-          Within 3 months of graduating my bootcamp, I was fortunate enough to get a Jr. Frontend Developer job at a company called InvestCloud! The job is going very well, and after only 5 months of employment, they asked me to become a mentor for other developers in the company! I can’t wait to see what the future holds and where this new career path will take me! Please enjoy looking through my portfolio of projects I've worked on!
+          Within 3 months of graduating my bootcamp, I was fortunate enough to get a Jr. Frontend Developer job at a company called InvestCloud, where after only 5 months of employment, they asked me to become a mentor for other developers in the company! Overall I spent 2 and a half years there, and am now ready for the next step in my development career!
+          </h5>
+          <h5 className="text-white mb-5" style={styles.main}>
+          Please enjoy looking through my portfolio of projects I've worked on!
           </h5>
         </div>
         <div className="col-12 ml-md-5 ml-lg-0 pl-md-5 pl-lg-0 col-lg-5 mt-lg-5 mt-xl-3 pt-lg-5 pt-xl-0 pr-lg-0">
             <img src={headshot} alt="DJ Headhsot" className="img-fluid" style={styles.headshot} />
-            <div className="mt-4 mb-xl-5 mr-md-5 pr-md-5 mr-lg-0 pr-lg-0 d-flex justify-content-center">
+            {/* <div className="mt-4 mb-xl-5 mr-md-5 pr-md-5 mr-lg-0 pr-lg-0 d-flex justify-content-center">
               <a href="https://investcloud.com" target="_blank"><img src={icLogo} alt="InvestCloud logo" className="img-fluid mr-md-4 mr-lg-0 icLogo"/></a>
-            </div>
+            </div> */}
         </div>
       </div>
       <div className="d-flex justify-content-center pb-5 mt-5 mt-lg-0">
--- src/Pages/Portfolio/Projects.js
+++ src/Pages/Portfolio/Projects.js
@@ -10,22 +10,22 @@ import todolist from "../../images/todolist.png";
 import bookTracker from "../../images/book-tracker.png";
 
 const projects = [
-    {
-        name: "Store Front",
-        deployed: "https://storefrontreact.herokuapp.com/",
-        image: storefront,
-        repo: "https://github.com/DJ620/store-front-react",
-        description: "Collaborated with a team of 4 other developers to build this React web application that allows store owners to create a fully-customized online store with a user-friendly interface. My contribution to this project was writing all of the back-end code to create the server and database, as well as implementing redux into the react code.",
-        tech: ["React", "Redux", "Redux-Persist", "Express", "MySQL", "Sequelize", "Cloudinary", "Axios", "Passport", "JSONwebtoken", "Bootstrap"]
-    },
-    {
-        name: "HARMøny",
-        deployed: "https://harmonythegame.herokuapp.com/",
-        image: harmony,
-        repo: "https://github.com/TChristensenDrumz/HARMony",
-        description: "Collaborated with a team of 4 other developers to build this browser-based, music-themed, roguelike game. My contribution to this project was building the back-end code, creating the server and database, writing the user authentication, and building the code for level transitions.",
-        tech: ["React", "Express", "MongoDB", "Mongoose", "JSONwebtoken", "Canvas", "Passport", "Axios"]
-    },
+    // {
+    //     name: "Store Front",
+    //     deployed: "https://storefrontreact.herokuapp.com/",
+    //     image: storefront,
+    //     repo: "https://github.com/DJ620/store-front-react",
+    //     description: "Collaborated with a team of 4 other developers to build this React web application that allows store owners to create a fully-customized online store with a user-friendly interface. My contribution to this project was writing all of the back-end code to create the server and database, as well as implementing redux into the react code.",
+    //     tech: ["React", "Redux", "Redux-Persist", "Express", "MySQL", "Sequelize", "Cloudinary", "Axios", "Passport", "JSONwebtoken", "Bootstrap"]
+    // },
+    // {
+    //     name: "HARMøny",
+    //     deployed: "https://harmonythegame.herokuapp.com/",
+    //     image: harmony,
+    //     repo: "https://github.com/TChristensenDrumz/HARMony",
+    //     description: "Collaborated with a team of 4 other developers to build this browser-based, music-themed, roguelike game. My contribution to this project was building the back-end code, creating the server and database, writing the user authentication, and building the code for level transitions.",
+    //     tech: ["React", "Express", "MongoDB", "Mongoose", "JSONwebtoken", "Canvas", "Passport", "Axios"]
+    // },
     {
         name: "Sell More Mattresses",
         deployed: "https://dj620.github.io/sell-more-mattresses/",
@@ -34,14 +34,14 @@ const projects = [
         description: "This is a mobile-responsive webpage built for the company 'Sell More Mattresses'. I sought out the opportunity to work on a real world website for an existing business, and partnered with a web designer to create the page. The designer provided me with " + <a href="https://drive.google.com/file/d/1lfkrvAtqBg6Vi8nKn2Xuz4PjvKNDtLXT/view?usp=sharing" target="_blank"> + "this " + </a> + "wireframe, which I used to then build the actual site.",
         tech: ["Bootstrap", "EmailJS", "Twitter API"]
     },
-    {
-        name: "Reading Tracker",
-        deployed: "https://djs-book-tracker.herokuapp.com/",
-        image: bookTracker,
-        repo: "https://github.com/DJ620/Book-tracker",
-        description: "This is an application I built to track reading progress. It uses the Google Books API to grab the data of any book, and allows you to log all of your reading sessions, including how much of the book you read, how long you read for, any quotes you want to jot down, and you can add your own notes. You can create your own account, or use the guest account: Username: Guest, Password: guest123",
-        tech: ["React", "Express", "MongoDB", "Mongoose", "JSONWebtoken", "React-Bootstrap", "Google Books API", "Axios", "Passport"]
-    },
+    // {
+    //     name: "Reading Tracker",
+    //     deployed: "https://djs-book-tracker.herokuapp.com/",
+    //     image: bookTracker,
+    //     repo: "https://github.com/DJ620/Book-tracker",
+    //     description: "This is an application I built to track reading progress. It uses the Google Books API to grab the data of any book, and allows you to log all of your reading sessions, including how much of the book you read, how long you read for, any quotes you want to jot down, and you can add your own notes. You can create your own account, or use the guest account: Username: Guest, Password: guest123",
+    //     tech: ["React", "Express", "MongoDB", "Mongoose", "JSONWebtoken", "React-Bootstrap", "Google Books API", "Axios", "Passport"]
+    // },
     {
         name: "RainShine",
         deployed: "https://dj620.github.io/rain-shine/",
@@ -50,14 +50,14 @@ const projects = [
         description: "This is a weather application that allows a user to get the current weather conditions, as well as a 5-day forecast for any city. Each city searched will be saved to the page, or can be removed by clicking the delete button.",
         tech: ["OpenWeather API", "jQuery", "Bootstrap"]
     },
-    {
-        name: "Build-A-Burger",
-        deployed: "https://blooming-plains-91984.herokuapp.com/",
-        image: burger,
-        repo: "https://github.com/DJ620/build-a-burger",
-        description: "This is a restaurant application that emulates the experience of building and then devouring a burger. It was created using the MVC paradigm.",
-        tech: ["Express", "Express Handlebars", "MySQL", "jQuery", "Bootstrap"]
-    },
+    // {
+    //     name: "Build-A-Burger",
+    //     deployed: "https://blooming-plains-91984.herokuapp.com/",
+    //     image: burger,
+    //     repo: "https://github.com/DJ620/build-a-burger",
+    //     description: "This is a restaurant application that emulates the experience of building and then devouring a burger. It was created using the MVC paradigm.",
+    //     tech: ["Express", "Express Handlebars", "MySQL", "jQuery", "Bootstrap"]
+    // },
     {
         name: "Code Quiz",
         deployed: "https://dj620.github.io/code-quiz/",
